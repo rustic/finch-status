dbd2ba7743f3814c682fe46e16af9bafd7976790
rustic <ichilegend@gmail.com>
2023-01-28 16:35:56 -0500

    Initial Commit

---
+++ finch-status/finch_statusApp.swift
@@ -0,0 +1,17 @@
+//
+//  finch_statusApp.swift
+//  finch-status
+//
+//  Created by Lee Myers on 1/28/23.
+//
+
+import SwiftUI
+
+@main
+struct finch_statusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
